3e7e55dcf7b95d377f867affa96aaf65b9c2aed6
enzoesper <enzoesper@hotmail.com>
2019-07-14 22:44:15 -0300

    adding the state and the corresponding function to obtain a certain selected line and draw it on the map

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
+import './App.css';
+import Drop from './DropDown';
+import ResMap from './ResMap';
+
 class App extends React.Component {
+
+  state = { linea: {} };
+  
+  onClickHandle = (linea) => {
+    this.setState({ linea });
+    console.log(this.state);
+  };
+
   render(){
-    return(
-      <div>App component!</div>
+    return (
+      <div className="map">
+        <ResMap linea={this.state.linea}/>
+        <Drop onClickHandle={this.onClickHandle}/>
+        <div className="card footer">
+          <p style={{padding:'2px'}} className="card-text">
+            Made by <a href="https://twitter.com/enzoesper" target="_blank" rel="noopener noreferrer"> enzoesper </a>
+          </p>
+        </div>
+      </div>
     )
   }
-};
+}
 
 export default App;
